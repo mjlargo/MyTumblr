3a846d6a55a5f7b8fad09439ccf5ef6a071625ba
LargoMarkJoseph <mjlargo015@gmail.com>
2023-05-12 22:28:02 +0800

    php login script added

--- account.php
+++ account.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+if(isset($_SESSION['ses_username'])===false){
+    header("Location: index.php?logfirst");
+}
+elseif(isset($_REQUEST['logout'])===true){
+    session_destroy();
+    header("Location: index.php?logout");
+}
+?>
+
 <!doctype html>
                         <html>
                             <head>
@@ -13,7 +25,7 @@
     transform: translateY(5rem)
 }
 .cover {
-    background-image: url('images/photo-profile-background.avif');
+    background-image: url('images/background.jpg');
     background-size: cover;
     background-repeat: no-repeat
 }
@@ -32,13 +44,13 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
+                    <div class="profile mr-3"><img src="images/profile.jpg" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Sign Out</a>
 
 
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">Ryan Clifford L. Perez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque PH
+                        <h4 class="mt-0 mb-0"><?php echo $_SESSION['ses_fullname']; ?></h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i><?php echo $_SESSION['ses_address']; ?>
                          </p>
                     </div>
                 </div>
@@ -46,36 +58,36 @@ body {
             <div class="bg-light p-4 d-flex justify-content-end text-center">
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">21,150</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
+                        <h5 class="font-weight-bold mb-0 d-block">69</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">756,450</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
+                        <h5 class="font-weight-bold mb-0 d-block">566,784</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">340</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
+                        <h5 class="font-weight-bold mb-0 d-block">999</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
                     </li>
                 </ul>
             </div>
 
             <div class="px-4 py-3">
-                <h5 class="mb-0">Expertise </h5>
+                <h5 class="mb-0">About Me</h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">Full-stack Web Developer</p>
-                    <p class="font-italic mb-0">Mobile App Developer</p>
-                    <p class="font-italic mb-0">Photographer/Videographer</p>
+                    <p class="font-italic mb-0">BS Information Technology</p>
+                    <p class="font-italic mb-0">2ND YEAR</p>
+                    <p class="font-italic mb-0">Integrative Programming & Technologies</p>
                 </div>
             </div>
 
             <div class="py-4 px-4">
                 <div class="d-flex align-items-center justify-content-between mb-3">
-                    <h5 class="mb-0">Recent Captures</h5><a href="#" class="btn btn-link text-muted">Show all</a>
+                    <h5 class="mb-0">My Photos</h5><a href="#" class="btn btn-link text-muted">Show all</a>
                 </div>
                 <div class="row">
                     
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/img1.jpg" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/img2.jpg" alt="" class="img-fluid rounded shadow-sm"></div>                
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/img3.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/img4.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
                 </div> 
             </div>
 
--- index.php
+++ index.php
@@ -1,17 +1,43 @@
+<?php
+SESSION_START();
+
+$acc_username="mjlargo";
+$acc_password="mjlargo123";
+$acc_fullanme="Mark Joseph Largo";
+$acc_address="Laylay Boac Marinduque";
+
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+if(isset($_REQUEST['login_button'])===true){
+	if($_REQUEST['form_username'] !=$acc_username){
+		header("Location: ".$url_add."?notexist");
+	}
+	else if($_REQUEST['form_username']== $acc_username && $_REQUEST['form_password'] != $acc_password){
+		header("Location: ".$url_add."?wrongpass");
+	}
+	else if($_REQUEST['form_username']== $acc_username && $_REQUEST['form_password'] == $acc_password){
+		header("Location: ".$url_add."?success");
+		$_SESSION['ses_username']= $acc_username;
+		$_SESSION['ses_password']= $acc_password;
+		$_SESSION['ses_fullname']= $acc_fullanme;
+		$_SESSION['ses_address']= $acc_address;
+	}
+
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
   	<title>MyTumblr Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	
 	<link rel="stylesheet" href="css/style.css">
-
 	</head>
 	<body>
 	<section class="ftco-section">
@@ -23,13 +49,43 @@
 		      		<span class="fa fa-user-o"></span>
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
-						
+
 						<form action="#" class="login-form">
+						<form method="POST" action="#" class="login-form">
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+		      			
+
+						<?php
+						if(isset($_REQUEST['notexist'])===true){
+							echo "<div class='alert alert-danger' role='alert'> Username does not exist</div>";
+						}
+						else if(isset($_REQUEST['wrongpass'])===true){
+							echo "<div class='alert alert-warning' role='alert'>Incorrect Password</div>";
+						}
+						else if(isset($_REQUEST['success'])===true){
+							echo "<div class='alert alert-success' role='alert'>Redirecting...</div>";
+							header("Refresh: 5; url=account.php");	
+
+						}
+						else if(isset($_REQUEST['logout'])===true){
+							echo "<div class='alert alert-info' role='alert'>Thank you..</div>";
+
+						}
+						else if(isset($_REQUEST['logfirst'])===true){
+							echo "<div class='alert alert-info' role='alert'>Please Log in first</div>";
+						}
+						else if(isset($_SESSION['ses_username'])===true){
+							echo "<div class='alert alert-warning' role='alert'> You're stile log in. Please
+							<a href='account.php'>click here </a> to proceed.</div>";
+						}
+
+						?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	             
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password"required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -44,6 +100,7 @@
 	            </div>
 	            <div class="form-group">
 	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -51,7 +108,6 @@
 			</div>
 		</div>
 	</section>
-
   <!--<script src="js/jquery.min.js"></script>
   <script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
@@ -59,4 +115,3 @@
 
 	</body>
 </html>
-
